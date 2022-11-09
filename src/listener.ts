9e1bc6ec24d781230dd70d87a8c851b24f6afc98
Aaron Lambley <aaron.lambley@gmail.com>
2022-11-09 12:23:57 +0000

    create Listener Class

--- src/listener.ts
+++ src/listener.ts
@@ -1,4 +1,4 @@
-import nats, { Message } from 'node-nats-streaming';
+import nats, { Message, Stan } from 'node-nats-streaming';
 import { randomBytes } from 'crypto';
 
 console.clear();
@@ -51,3 +51,55 @@ stan.on('connect', () => {
 // watch for interrupt or terminate signals and trigger close event
 process.on('SIGINT', () => stan.close());
 process.on('SIGTERM', () => stan.close());
+
+abstract class Listener {
+  // abstract classes for subclasses to define
+  abstract subject: string;
+  abstract queueGroupName: string;
+  abstract onMessage(data: any, msg: Message): void
+
+  // asssumes instance of client has already successfully connected
+  private client: Stan;
+  protected ackWait = 5 * 1000; // 5000ms = 5s default ackWait
+
+  constructor(client: Stan) {
+    this.client = client;
+  }
+
+  // subscription options function
+  // set manual acknowledgement mode to true - in case db is down for example
+  // setDeliveryAllAvailable - sends all previously sent events - one approach to account for service downtime
+  subscriptionOptions() {
+    return this.client
+      .subscriptionOptions()
+      .setDeliverAllAvailable()
+      .setManualAckMode(true)
+      .setAckWait(this.ackWait)
+      .setDurableName(this.queueGroupName);
+  }
+
+  // setup subscription
+  listen() {
+    const subscription = this.client.subscribe(
+      this.subject,
+      this.queueGroupName,
+      this.subscriptionOptions()
+    );
+
+    subscription.on('message', (msg: Message) => {
+      console.log(`Message received: ${this.subject} | ${this.queueGroupName}`);
+
+      const parsedData = this.parseMessage(msg);
+      this.onMessage(parsedData, msg);
+    });
+  }
+
+  parseMessage(msg: Message) {
+    const data = msg.getData();
+
+    // parse string or buffer
+    return typeof data === 'string'
+      ? JSON.parse(data)
+      : JSON.parse(data.toString('utf8'));
+  }
+}
