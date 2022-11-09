63ab6768ab34f18158aaa4f86bbc848f8f10107c
Aaron Lambley <aaron.lambley@gmail.com>
2022-11-09 12:56:17 +0000

    add Event interface and implement generic Event in type declarations

--- src/events/base-listener.ts
+++ src/events/base-listener.ts
@@ -1,10 +1,16 @@
 import { Message, Stan } from 'node-nats-streaming';
+import { Subjects } from './subjects';
 
-export abstract class Listener {
+interface Event {
+  subject: Subjects;
+  data: any;
+}
+
+export abstract class Listener<T extends Event> {
   // abstract classes for subclasses to define
-  abstract subject: string;
+  abstract subject: T['subject'];
   abstract queueGroupName: string;
-  abstract onMessage(data: any, msg: Message): void;
+  abstract onMessage(data: T['data'], msg: Message): void;
 
   // asssumes instance of client has already successfully connected
   private client: Stan;
